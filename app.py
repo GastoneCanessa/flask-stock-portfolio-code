cbbb813ae1095afc914a74da7d424894cf676a0a
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2023-03-20 21:37:00 +0100

    add_forms

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, escape, render_template
+from flask import Flask, escape, render_template, request
+from pydantic import BaseModel, validator, ValidationError
 
 app = Flask(__name__)
+ 
+
+class StockModel(BaseModel):
+    """Class for parsing new stock data a form."""
+    stock_symbol: str
+    number_of_shares: int
+    purchase_price: float
+
+    @validator('stock_symbol')
+    def stock_symbol_check(cls, value):
+        if not value.isalpha() or len(value) > 5:
+            raise ValueError('Stock symbol must be 1-5 characters')
+        return value.upper()    
 
 
 @app.route('/')
@@ -10,4 +24,24 @@ def index():
 
 @app.route('/about')   
 def about():
-    return render_template('about.html', company_name='TestDriven.io') 
+    return render_template('about.html', company_name='TestDriven.io')
+
+
+@app.route('/add_stock', methods=['GET', 'POST'])
+def add_stock():
+    if request.method == 'POST':
+        # Print the form data to the console
+        for key, value in request.form.items():
+            print(f'{key}: {value}')
+
+        try:
+            stock_data = StockModel(
+                stock_symbol=request.form['stock_symbol'],
+                number_of_shares=request.form['number_of_shares'],
+                purchase_price=request.form['purchae_price']
+            )
+            print(stock_data)
+        except ValidationError as e:
+            print(e)
+
+    return render_template('add_stock.html')     
