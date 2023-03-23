a0eeac21eae2a810f4c0d68193f53787620e9aee
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2023-03-23 16:12:14 +0100

    Applicaton_Factory

--- app.py
+++ app.py
@@ -5,32 +5,40 @@ import logging
 from flask.logging import default_handler
 from logging.handlers import RotatingFileHandler
 import os
-
-app = Flask(__name__) 
-
-# Configure the Flask application
-config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
-app.config.from_object(config_type)
-
-app.logger.removeHandler(default_handler)  # serve per non stampare in console i logger
-
-# Logging Configuration
-file_handler = RotatingFileHandler('instance/flask-stock-portfolio.log',
-                                   maxBytes=16384,
-                                   backupCount=20)  # server per creare un altro file di logger quando le dimensioni diventano troppo grandi
-file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')  
-file_handler.setFormatter(file_formatter)  
-file_handler.setLevel(logging.INFO)
-app.logger.addHandler(file_handler)
-
-# Log that the Flask application is starting
-app.logger.info('Starting the Flask Stock Portfolio App...')
+from project import create_app
 
 
-# Import the blueprints
-from project.stocks import stocks_blueprint
-from project.users import users_blueprint
 
-# Register the blueprints
-app.register_blueprint(stocks_blueprint)
-app.register_blueprint(users_blueprint, url_prefix='/users')
+# Call the application factory function to construct a Flask application
+# instance using the development configuration
+app = create_app()
+
+
+# app = Flask(__name__) 
+# # Configure the Flask application
+# config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
+# app.config.from_object(config_type)
+
+# app.logger.removeHandler(default_handler)  # serve per non stampare in console i logger
+
+# # Logging Configuration
+# file_handler = RotatingFileHandler('instance/flask-stock-portfolio.log',
+#                                    maxBytes=16384,
+#                                    backupCount=20)  # server per creare un altro file di logger quando le dimensioni diventano troppo grandi
+
+# file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')  
+# file_handler.setFormatter(file_formatter)  
+# file_handler.setLevel(logging.INFO)
+# app.logger.addHandler(file_handler)
+
+# # Log that the Flask application is starting
+# app.logger.info('Starting the Flask Stock Portfolio App...')
+
+
+# # Import the blueprints
+# from project.stocks import stocks_blueprint
+# from project.users import users_blueprint
+
+# # Register the blueprints
+# app.register_blueprint(stocks_blueprint)
+# app.register_blueprint(users_blueprint, url_prefix='/users')
