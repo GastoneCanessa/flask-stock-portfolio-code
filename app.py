054861fd34f4bddab0075fc4d31ae18b42394032
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2023-03-23 13:49:35 +0100

    blueprints

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import logging
 from flask.logging import default_handler
 from logging.handlers import RotatingFileHandler
 
-app = Flask(__name__) #we
+app = Flask(__name__) 
 
 app.logger.removeHandler(default_handler)  # serve per non stampare in console i logger
 
@@ -24,60 +24,10 @@ app.logger.addHandler(file_handler)
 app.logger.info('Starting the Flask Stock Portfolio App...')
 
 
-class StockModel(BaseModel):
-    """Class for parsing new stock data a form."""
-    stock_symbol: str
-    number_of_shares: int
-    purchase_price: float
+# Import the blueprints
+from project.stocks import stocks_blueprint
+from project.users import users_blueprint
 
-    @validator('stock_symbol')
-    def stock_symbol_check(cls, value):
-        if not value.isalpha() or len(value) > 5:
-            raise ValueError('Stock symbol must be 1-5 characters')
-        return value.upper()    
-
-
-@app.route('/')
-def index():
-    app.logger.info('Calling the index() function.')  # NEW!
-    return render_template('index.html')
-
-
-@app.route('/about')   
-def about():
-    flash('Thanks for learning about this site!', 'info')
-    return render_template('about.html', company_name='TestDriven.io')
-
-
-@app.route('/add_stock', methods=['GET', 'POST'])
-def add_stock():
-    if request.method == 'POST':
-        # Print the form data to the console
-        for key, value in request.form.items():
-            print(f'{key}: {value}')
-
-        try:
-            stock_data = StockModel(
-                stock_symbol=request.form['stock_symbol'],
-                number_of_shares=request.form['number_of_shares'],
-                purchase_price=request.form['purchase_price']
-            )
-            print(stock_data)
-
-            # Save the form data to the session object
-            session['stock_symbol'] = stock_data.stock_symbol
-            session['number_of_shares'] = stock_data.number_of_shares
-            session['purchase_price'] = stock_data.purchase_price
-            flash(f"Added new stock ({stock_data.stock_symbol})!", 'success')  
-            app.logger.info(f"Added new stock ({request.form['stock_symbol']})!") 
-
-            return redirect(url_for('list_stocks'))
-        except ValidationError as e:
-            print(e)
-
-    return render_template('add_stock.html')     
-
-
-@app.route('/stocks/')
-def list_stocks():
-    return render_template('stocks.html')
+# Register the blueprints
+app.register_blueprint(stocks_blueprint)
+app.register_blueprint(users_blueprint, url_prefix='/users')
