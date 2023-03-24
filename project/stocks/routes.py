8fc738cf0433ffc7f33e1e0a9a3ed2546bcee18a
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2023-03-24 17:49:54 +0100

    CLI-Commands

--- project/stocks/routes.py
+++ project/stocks/routes.py
@@ -3,6 +3,7 @@ from flask import current_app, render_template, request, session, flash, redirec
 from pydantic import BaseModel, validator, ValidationError
 from project.models import Stock
 from project import database
+import click
 
 
 class StockModel(BaseModel):
@@ -71,3 +72,30 @@ def add_stock():
 def list_stocks():
     stocks = Stock.query.order_by(Stock.id).all()
     return render_template('stocks/stocks.html', stocks=stocks)
+
+
+# ------------
+# CLI Commands
+# ------------
+
+@stocks_blueprint.cli.command('create_default_set')
+def create_default_set():
+    """Create three new stocks and add them to the database"""
+    stock1 = Stock('HD', '25', '247.29')
+    stock2 = Stock('TWTR', '230', '31.89')
+    stock3 = Stock('DIS', '65', '118.77')
+    database.session.add(stock1)
+    database.session.add(stock2)
+    database.session.add(stock3)
+    database.session.commit()
+
+
+@stocks_blueprint.cli.command('create')
+@click.argument('symbol')
+@click.argument('number_of_shares')
+@click.argument('purchase_price')
+def create(symbol, number_of_shares, purchase_price):
+    """Create a new stock and add it to the database"""
+    stock = Stock(symbol, number_of_shares, purchase_price)
+    database.session.add(stock)
+    database.session.commit()    
